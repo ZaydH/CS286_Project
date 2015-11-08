272c98c9592058ddfd8a3f79ec4f3dd594bf1fb2
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-08 07:12:19 -0800

    Fixing bugs in the modified EnsembleReducer code.

--- EnsembleClassifier/EnsembleReducer.java
+++ EnsembleClassifier/EnsembleReducer.java
@@ -7,85 +7,45 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 
-@SuppressWarnings("unused")
 public class EnsembleReducer  extends Reducer <Text,Text,Text,Text> {
-	String[] tempString;
-	float tempSepalLength, tempSepalWidth, tempPetalLength, tempPetalWidth;
-	float totalSepalLength, totalSepalWidth, totalPetalLength, totalPetalWidth;
-	float minSepalLength, maxSepalLength, meanSepalLength, minSepalWidth, maxSepalWidth, meanSepalWidth, minPetalLength, maxPetalLength, meanPetalLength, minPetalWidth, maxPetalWidth, meanPetalWidth;
-
-	
-	private static final boolean USE_STRING_TOKENIZER = false;
 	
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-		// Initialize the variable values
-		minSepalLength = minPetalLength = minSepalWidth = minPetalWidth = Float.MAX_VALUE;
-		maxSepalLength = maxPetalLength = maxSepalWidth = maxPetalWidth = Float.MIN_VALUE;
-		totalSepalLength = totalSepalWidth = totalPetalLength = totalPetalWidth = 0;
-		int count = 0;
+		double[] cuisineScore = null;
+		int actualCuisineType = -1;
 
 		// Iterate through all mapped value lines.
 		for(Text value: values) {
 			
-			if(!USE_STRING_TOKENIZER){
-				// TODO use String split() method to split value and assign to tempString
-				// In mapper, fields are underscore separated.
-				String[] tempString = value.toString().split("_");
-	
-				// TODO convert tempString elements to temp sepal/petal length/width vars
-				tempSepalLength = Float.parseFloat(tempString[0]);
-				tempSepalWidth = Float.parseFloat(tempString[1]);
-				tempPetalLength = Float.parseFloat(tempString[2]);
-				tempPetalWidth = Float.parseFloat(tempString[3]);  
+			// Parse the key's values.
+			String[] splitStr = value.toString().split(",");
+			
+			// Initialize the class values for the cuisine score and actual cuisine type
+			if(cuisineScore == null){
+				actualCuisineType = Integer.parseInt(splitStr[1]);
+				cuisineScore = new double[splitStr.length - 2];
+				for(int i = 0; i < cuisineScore.length; i++)
+					cuisineScore[i] = 0;
 			}
-			else{
-				StringTokenizer token = new StringTokenizer(value.toString(), "_");
-				// TODO convert tempString elements to temp sepal/petal length/width vars
-				tempSepalLength = Float.parseFloat(token.nextToken());
-				tempSepalWidth = Float.parseFloat(token.nextToken());
-				tempPetalLength = Float.parseFloat(token.nextToken());
-				tempPetalWidth = Float.parseFloat(token.nextToken());
-			}
-    	  
-			// TODO determine if you have min/max sepal/petal length/widths and assign to min/max sepal/petal length/widths accordingly
-			// Update the maximum parameters.
-			if(tempSepalLength > maxSepalLength) maxSepalLength = tempSepalLength;
-			if(tempSepalWidth > maxSepalWidth) maxSepalWidth = tempSepalWidth;
-			if(tempPetalLength > maxPetalLength) maxPetalLength = tempPetalLength;
-			if(tempPetalWidth > maxPetalWidth) maxPetalWidth = tempPetalWidth;
-			// Update the Minimum parameters if applicable
-			if(tempSepalLength < minSepalLength) minSepalLength = tempSepalLength;
-			if(tempSepalWidth < minSepalWidth) minSepalWidth = tempSepalWidth;
-			if(tempPetalLength < minPetalLength) minPetalLength = tempPetalLength;
-    	  	if(tempPetalWidth < minPetalWidth) minPetalWidth = tempPetalWidth;
 
-    	  	// TODO calculate running totals for sepal/petal length/widths for use in calculation of means
-    	  	totalPetalLength += tempPetalLength;
-    	  	totalPetalWidth += tempPetalWidth;
-    	  	totalSepalLength += tempSepalLength;
-    	  	totalSepalWidth += tempSepalWidth;
-
-    	  	// TODO increment counter for use in calculation of means
-    	  	count++;
+			// Aggregate the class scores
+			for(int i = 2; i < splitStr.length; i++)
+				cuisineScore[i-2] += Double.parseDouble(splitStr[i]);
 
 		} 
-     
-		// TODO calculate mean sepal/petal length/width 
-		meanPetalLength = (totalPetalLength)/count;
-		meanPetalWidth = (totalPetalWidth)/count;
-		meanSepalLength = (totalSepalLength)/count;
-		meanSepalWidth = (totalSepalWidth)/count;
-       
-		// TODO generate string output per the requirement
-		// minSepalLength\tmaxSepalLength\tmeanSepalLength\t ...
-		String output =   minSepalWidth  + "\t" + maxSepalWidth  + "\t" + meanSepalWidth  + "\t"
-      		   		    + minSepalLength + "\t" + maxSepalLength + "\t" + meanSepalLength + "\t" 
-      		   		    + minPetalWidth  + "\t" + maxPetalWidth  + "\t" + meanPetalWidth  + "\t" 
-    		   		    + minPetalLength + "\t" + maxPetalLength + "\t" + meanPetalLength;
 
-		// TODO emit output to context
-		context.write(key, new Text(output));
+		// Determine the best cuisine
+		int bestCuisine = 0;
+		for(int i = 1; i < cuisineScore[i]; i++){
+		
+			if(cuisineScore[bestCuisine] < cuisineScore[i])
+				bestCuisine = i;
+
+		}
+		
+		// Key - Recipe ID
+		// Value - Comma separated list.  Element 0 is the actual cuisine number. Element 1 is the predicted cuisine number	
+		context.write(key, new Text( actualCuisineType + "," + bestCuisine));
 
 	}
 }
