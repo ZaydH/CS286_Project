caea2cb0def6156f53177719ca60427c9de6bcc4
ZaydH <zayd.hammoudeh@sjsu.edu>
2015-11-07 23:03:59 -0800

    Uploading the training and test set data.  Also uploading the preprocessor file information.

--- RecipePreprocessor/src/RecipePreprocessor/Preprocessor.java
+++ RecipePreprocessor/src/RecipePreprocessor/Preprocessor.java
@@ -8,7 +8,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import com.google.gson.Gson;
@@ -19,7 +21,7 @@ public class Preprocessor {
 	 * 
 	 * @param args	
 	 * 				- args[0] - Path to the full dataset
-	 * 				- args[1] - Directory for outputting the training set data.
+	 * 				- args[1] - File outputting the training set data.
 	 * 				- args[2] - Directory for outputting the test set data.
 	 * 				- args[3] - Number of test set files.
 	 * 				- args[4] - Filename for the cuisine type mapper
@@ -37,8 +39,8 @@ public class Preprocessor {
 		// Parse the input arguments
 		int k = 0;
 		String trainingFile = args[k++];
-		String trainingSetDir = args[k++];
-		String testSetDir = args[k++];
+		String trainingSetFilePath = args[k++];
+		String testSetDir = args[k++]; 
 		int numbTestFiles = Integer.parseInt(args[k++]);
 		String cuisinesFilePath = args[k++];
 		
@@ -46,28 +48,35 @@ public class Preprocessor {
 		BufferedReader fileIn = new BufferedReader(new FileReader(trainingFile));
 
 		Gson gson = new Gson();
-		Recipe[] recipeArray = gson.fromJson(fileIn, Recipe[].class);
+		Recipe[] recipeArr = gson.fromJson(fileIn, Recipe[].class);
 		
 		// Perform Fisher-Yates Shuffle
 		Random rnd = new Random();
-		for(int i = 0; i < recipeArray.length; i++){
-			int loc = rnd.nextInt(recipeArray.length - i); // Get the index to swap
-			Recipe tempRecipe = recipeArray[recipeArray.length - 1 - i];
-			recipeArray[recipeArray.length - 1 - i] = recipeArray[loc];
-			recipeArray[loc] = tempRecipe;
+		for(int i = 0; i < recipeArr.length; i++){
+			int loc = rnd.nextInt(recipeArr.length - i); // Get the index to swap
+			Recipe tempRecipe = recipeArr[recipeArr.length - 1 - i];
+			recipeArr[recipeArr.length - 1 - i] = recipeArr[loc];
+			recipeArr[loc] = tempRecipe;
 		}
 		
 		// Process the ingredient in formation
+		Hashtable<String, Integer> ingredientIDs = new Hashtable<String, Integer>();
 		ArrayList<String> cuisines = new ArrayList<String>();	// This will be used to normalize the cuisine types to a string
-		for(Recipe tempRecipe : recipeArray){
+		for(Recipe tempRecipe : recipeArr){
 			// Clean ingredients
 			String[] ingredientList = tempRecipe.getIngredients();	
-			int m=0;
 			for(int j = 0; j < ingredientList.length; j++ ){
 				String filteredString = ingredientCleaner(ingredientList[j]);
+				
 				// Verify the filtered ingredient is not empty
 				if(filteredString.length() > 0)
 					ingredientList[j] = filteredString;
+				
+				// Check if the ingredient has an ID number. if not assign it the next open ID
+				if(ingredientIDs.get(ingredientList[j]) == null){
+					Integer id = ingredientIDs.size();
+					ingredientIDs.put(ingredientList[j], id);
+				}
 			}
 			tempRecipe.updateIngredients(ingredientList);
 			
@@ -78,9 +87,38 @@ public class Preprocessor {
 		
 		// Sort the cuisines in alphabetical order
 		Collections.sort(cuisines);
-		printCuisineInfoToAFile(cuisines, cuisinesFilePath);
+		printCuisineInfoToAFile(cuisinesFilePath, cuisines);
 		
+		// Print the training set data
+		int trainingSetSize = (int)(2.0 / 3 * recipeArr.length);
+		printRecipesToFile(trainingSetFilePath, recipeArr, 0, trainingSetSize, ingredientIDs, cuisines);
 		
+		// Calculate the number of records per file
+		int recordsPerFile = (recipeArr.length - trainingSetSize) / numbTestFiles;
+		
+		// Verify the test set directory contains a terminating slash if applicable
+		if( !testSetDir.endsWith("\\") && !testSetDir.endsWith("/") ){
+			if(testSetDir.contains("\\"))
+				testSetDir = testSetDir.concat("\\");
+			else if(testSetDir.contains("/"))
+				testSetDir = testSetDir.concat("/");
+		}
+		// Output the test set information
+		for(int i = 0; i < numbTestFiles; i++){
+		
+			
+			// Add preceding zeros to the file number
+			String fileNumber = ("00000" + i).substring( Integer.toString(i).length());
+			String filePath = testSetDir + "test_set_" + fileNumber + ".txt";
+			
+			// Will need to handle the last file differently since it will need to include the last record. 
+			if(i != numbTestFiles - 1)
+				printRecipesToFile(filePath, recipeArr, trainingSetSize + i*recordsPerFile, 
+								   trainingSetSize + (i+1)*recordsPerFile, ingredientIDs, cuisines);
+			else
+				printRecipesToFile(filePath, recipeArr, trainingSetSize + i*recordsPerFile, 
+								   recipeArr.length, ingredientIDs, cuisines);
+		}
 	}
 	
 	
@@ -166,7 +204,7 @@ public class Preprocessor {
 	 * @param filePath		- String - Path to write the cuisineinfo to.
 	 * @throws IOException
 	 */
-	public static void printCuisineInfoToAFile(List<String> cuisines, String filePath) throws IOException{
+	public static void printCuisineInfoToAFile(String filePath, List<String> cuisines ) throws IOException{
 		
 		BufferedWriter fileOut = new BufferedWriter(new FileWriter(filePath));
 		
@@ -177,6 +215,50 @@ public class Preprocessor {
 				fileOut.newLine();
 			fileOut.write(cuisines.get(i) + "," + i);
 
+		}
+		fileOut.close(); // Close the file.
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Used to print the recipe information to a file.
+	 * 
+	 * @param filePath			String - Path to print the file to.
+	 * @param recipes			Recipe[] - Array of Recipes
+	 * @param startLoc			int - Inclusive first index of the recipes array to print to the file
+	 * @param endLoc			int - Exclsuive last index of the recipes array to print to the file
+	 * @param ingredientIDs		Map<String, Integer> - Converts String objects (i.e. ingredients) to integers
+	 * @param cuisineIDs		List<String> Array of the cuisine type names
+	 * @throws IOException
+	 */
+	public static void printRecipesToFile(String filePath, Recipe[] recipes, int startLoc, int endLoc, 
+										  Map<String, Integer>ingredientIDs, List<String> cuisineIDs) throws IOException{
+		
+		BufferedWriter fileOut = new BufferedWriter(new FileWriter(filePath));
+		
+		// Print the dataset information settings at the top of the file.
+		fileOut.write(cuisineIDs.size() + "," + ingredientIDs.size());
+		
+		// Print one recipe at a time.
+		for(int i = startLoc; i < endLoc; i++){
+			// Put a blank line before new lines to prevent an empty blank line at the of the file.
+			fileOut.newLine();
+			StringBuffer sb = new StringBuffer();
+			
+			// Extract the temporary recipe
+			Recipe tempRecipe = recipes[i];
+			
+			// Build the output string
+			sb.append( tempRecipe.id + "," + cuisineIDs.indexOf(tempRecipe.getCuisine()) ); // Add the recipe and cuisine ID numbers 
+			
+			// Append the ingredient list
+			for(String ingredient : tempRecipe.getIngredients())
+				sb.append("," + ingredientIDs.get(ingredient));
+		
+			// Write the string to a file.
+			fileOut.write(sb.toString());
 		}
 		fileOut.close(); // Close the file.
 		
