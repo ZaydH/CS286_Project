8bcbe0a8a14c0705f1c1eb8dbdfcfdee193d2cd0
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-08 04:09:18 -0800

    Multiple fixes getting the oozie flow working.  Still a work in progress.  Just checkpointing for now.

--- oozie_flow/oozie_flow.bash
+++ oozie_flow/oozie_flow.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://localhost:11000/oozie" -config /user/user01/CS286_Project/oozie_flow/job.properties -run
+export OOZIE_URL='http://mapr1node:11000/oozie'
+export OOZIE_HOME=/opt/mapr/oozie/oozie-4.1.0
+export PATH=$PATH:$OOZIE_HOME/bin
+
+/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://localhost:11000/oozie" -config /user/user01/oozie_flow/job.properties -run
--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export OOZIE_DIR=oozie_flow
+export DATA_DIR=data
 export PREPROCESSOR_DIR=RecipePreprocessor
 
 # Remake the oozie lib directory from scratch.
@@ -9,3 +10,11 @@ mkdir $OOZIE_DIR/lib
 
 # Copy the processor jar into the oozie lib directory.
 cp $PREPROCESSOR_DIR/Preprocessor.jar $OOZIE_DIR/lib
+
+hadoop fs -rmr /user/user01/$OOZIE_DIR
+hadoop fs -copyFromLocal $OOZIE_DIR /user/user01/
+hadoop fs -mkdir -p /user/user01/$DATA_DIR/training_set
+hadoop fs -mkdir -p /user/user01/$DATA_DIR/test_set
+
+rm -f /user/user01/$DATA_DIR/train.json
+cp /home/user01/CS286_Project/RecipePreprocessor/train.json /user/user01/$DATA_DIR
