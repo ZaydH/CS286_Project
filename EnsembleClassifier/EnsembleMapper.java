db4f31834a604244016b0e2482f3502e409ee133
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-08 06:53:19 -0800

    Reworking EnsembleMapper for the ensemble classifier.

--- EnsembleClassifier/EnsembleMapper.java
+++ EnsembleClassifier/EnsembleMapper.java
@@ -5,7 +5,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+
+
 
 public class EnsembleMapper extends Mapper <LongWritable,Text,Text,Text> {
 	
@@ -13,49 +14,30 @@ public class EnsembleMapper extends Mapper <LongWritable,Text,Text,Text> {
 	
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
     
-		if(!USE_STRING_TOKENIZER){
-			// TODO create array of string tokens from record assuming space-separated fields using split() method of String class
-			String[] tempString = value.toString().split("\\s+");
+		// Split the input text by commas.
+		String[] splitValues = value.toString().split(",");
+		
+		double sumOfCuisineVals = 0;
+		for(int j = 2; j < splitValues.length; j++)
+			sumOfCuisineVals = Double.parseDouble(splitValues[j]);
+		
+		
+
+		StringBuffer sb = new StringBuffer();
+
+		// Add the recipe ID and stored label
+		sb.append( splitValues[0] + "," + splitValues[1] );
+		
+		// Build the cuisine values
+		for(int j = 2; j < splitValues.length; j++){
+			sb.append(",");
+			double normalizedVal = Double.parseDouble(splitValues[j])/sumOfCuisineVals;
+			sb.append(normalizedVal);
 			
-			// TODO pull out sepal length from columns var
-			String sepalLength = tempString[0];
-	
-			//  TODO pull out sepal width from columns var
-			String sepalWidth = tempString[1];
-			
-			// TODO pull out petal length from columns var
-			String petalLength = tempString[2];
-			
-			// TODO pull out petal width from columns var
-			String petalWidth = tempString[3];
-	
-			// TODO pull out flower id from columns var
-			String flowerID = tempString[4];
-			// TODO write output to context as key-value pair where key is 
-			// flowerId and value is underscore-separated concatenation of 
-			// sepal/petal length/widths
-			String outputValue =   sepalLength + "_" + sepalWidth + "_"
-								 + petalLength + "_" + petalWidth;
-			context.write(new Text(flowerID), new Text(outputValue));
 		}
-		else{
-			// TODO create array of string tokens from record assuming space-separated fields using split() method of String class
-			StringTokenizer token = new StringTokenizer(value.toString(), "\t");
-			String[] stringPieces = new String[4];
-			StringBuilder strBuild = new StringBuilder();
-			int cnt;
-			
-			// Get the tokens and construct the output string.
-			for(cnt = 0; cnt < stringPieces.length; cnt++){
-				if(cnt!=0) strBuild.append("_");
-				stringPieces[cnt] = token.nextToken();
-				strBuild.append(stringPieces[cnt]);
-			}
-			// TODO pull out flower id from columns var
-			String flowerID = token.nextToken();
-			
-			// TODO write output to context as key-value pair where key is 
-			context.write(new Text(flowerID), new Text(strBuild.toString()));
-		}
+
+		// Key is the recipe ID
+		// Value is the normalized class values.	
+		context.write(new Text(splitValues[0]), new Text(sb.toString()));
    }
 }
