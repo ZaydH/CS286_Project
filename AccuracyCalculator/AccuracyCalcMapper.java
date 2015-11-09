289311fea8493e80cc5bef6863ab91585152fa96
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-09 00:39:27 -0800

    Creating placeholder files for the accuracy calculator.

---
+++ AccuracyCalculator/AccuracyCalcMapper.java
@@ -0,0 +1,42 @@
+package Accuracy;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+
+import java.io.IOException;
+
+
+
+public class AccuracyCalcMapper extends Mapper <LongWritable,Text,Text,Text> {
+	
+	private static final boolean USE_STRING_TOKENIZER = false;
+	
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    
+		// Split the input text by commas.
+		String[] splitValues = value.toString().split(",");
+		
+		double sumOfCuisineVals = 0;
+		for(int j = 2; j < splitValues.length; j++)
+			sumOfCuisineVals += Double.parseDouble(splitValues[j]);
+		
+		// Build the output string.
+		StringBuffer sb = new StringBuffer();
+
+		// Add the recipe ID and stored label
+		sb.append( splitValues[0] + "," + splitValues[1] );
+		
+		// Build the cuisine values
+		for(int j = 2; j < splitValues.length; j++){
+			sb.append(",");
+			double normalizedVal = Double.parseDouble(splitValues[j])/sumOfCuisineVals;
+			sb.append(normalizedVal);
+			
+		}
+
+		// Key is the recipe ID
+		// Value is the normalized class values.	
+		context.write(new Text(splitValues[0]), new Text(sb.toString()));
+   }
+}
---
+++ AccuracyCalculator/AccuracyCalcReducer.java
@@ -0,0 +1,50 @@
+package Accuracy;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.FloatWritable;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class AccuracyCalcReducer  extends Reducer <Text,Text,Text,Text> {
+	
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+		double[] cuisineScore = null;
+		int actualCuisineType = -1;
+
+		// Iterate through all mapped value lines.
+		for(Text value: values) {
+			
+			// Parse the key's values.
+			String[] splitStr = value.toString().split(",");
+			
+			// Initialize the class values for the cuisine score and actual cuisine type
+			if(cuisineScore == null){
+				actualCuisineType = Integer.parseInt(splitStr[1]);
+				cuisineScore = new double[splitStr.length - 2];
+				for(int i = 0; i < cuisineScore.length; i++)
+					cuisineScore[i] = 0;
+			}
+
+			// Aggregate the class scores
+			for(int i = 2; i < splitStr.length; i++)
+				cuisineScore[i-2] += Double.parseDouble(splitStr[i]);
+
+		} 
+
+		// Determine the best cuisine
+		int bestCuisine = 0;
+		for(int i = bestCuisine+1; i < cuisineScore.length; i++){
+			if(cuisineScore[bestCuisine] < cuisineScore[i])
+				bestCuisine = i;
+
+		}
+		
+		// Key - Recipe ID
+		// Value - Comma separated list.  Element 0 is the actual cuisine number. Element 1 is the predicted cuisine number	
+		context.write(key, new Text( actualCuisineType + "," + bestCuisine));
+
+	}
+}
