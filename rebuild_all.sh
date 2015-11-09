e249e5a185dd1f29129d377727625453bfd8a3c6
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-08 22:39:46 -0800

    Cleaned up the rebuild scripting with the copy over capability working.

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -4,6 +4,8 @@ export OOZIE_DIR=oozie_flow
 export DATA_DIR=data
 export PREPROCESSOR_DIR=RecipePreprocessor
 
+export ENSEMBLE_DIR=EnsembleClassifier
+
 # Remake the oozie lib directory from scratch.
 rm -rf /home/user01/CS286_Project/$OOZIE_DIR/lib 
 mkdir /home/user01/CS286_Project/$OOZIE_DIR/lib
@@ -11,11 +13,20 @@ mkdir /home/user01/CS286_Project/$OOZIE_DIR/lib
 # Copy the processor jar into the oozie lib directory.
 cp /home/user01/CS286_Project/$PREPROCESSOR_DIR/Preprocessor.jar /home/user01/CS286_Project/$OOZIE_DIR/lib
 
+# Copy over the source data set.
+rm -rf /user/user01/$DATA_DIR
+mkdir -p /user/user01/$DATA_DIR
+cp /home/user01/CS286_Project/RecipePreprocessor/train.json /user/user01/$DATA_DIR
+
+# Copy over the ensemble testing data
+rm -rf /user/user01/data/mvdm
+cp -r /home/user01/CS286_Project/data/mvdm /user/user01/$DATA_DIR
+cp -r /home/user01/CS286_Project/data/overlap /user/user01/$DATA_DIR
+cp -r /home/user01/CS286_Project/data/naive_bayes /user/user01/$DATA_DIR
+
+# Copy the ensemble jar into the oozie lib directory.
+cp /home/user01/CS286_Project/$ENSEMBLE_DIR/ensemble.jar /home/user01/CS286_Project/$OOZIE_DIR/lib
+
+# Last Step - Copy the local Oozie flow directory to the /user/user01 directory.
 hadoop fs -rmr /user/user01/$OOZIE_DIR
 hadoop fs -copyFromLocal /home/user01/CS286_Project/$OOZIE_DIR /user/user01/
-mkdir -p /user/user01/$DATA_DIR/training_set
-mkdir -p /user/user01/$DATA_DIR/test_set
-mkdir -p /user/user01/$DATA_DIR/cuisines
-
-rm -f /user/user01/$DATA_DIR/train.json
-cp /home/user01/CS286_Project/RecipePreprocessor/train.json /user/user01/$DATA_DIR
