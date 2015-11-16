91a66c5bf22575f57e3e1c70747528984e4706af
ec2-user <ec2-user@ip-10-0-0-139.us-west-1.compute.internal>
2015-11-16 03:21:57 -0500

    Fixing a couple of bugs in the job_aws.properties file for AWS.

--- oozie_flow/oozie_flow_aws.bash
+++ oozie_flow/oozie_flow_aws.bash
@@ -4,7 +4,7 @@ export OOZIE_URL='http://ip-10-0-0-139.us-west-1.compute.internal:11000/oozie'
 export OOZIE_HOME=/opt/mapr/oozie/oozie-4.2.0
 export PATH=$PATH:$OOZIE_HOME/bin
 
-$OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /user/ec2-user/oozie_flow/job_aws.properties -run
+$OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /home/ec2-user/CS286_Project/oozie_flow/job_aws.properties -run
 
 # Check job status. Replace <JobName> with the actual job name
 #$OOZIE_HOME/bin/oozie job -oozie $OOZIE_URL/oozie -info <JobName>
