d31e553e08d407bfb33f7058f890ad85da8fda13
Shubhangi <shubhangi.rakhonde@gmai.com>
2015-11-21 13:48:22 -0800

    Sets the training file path in configuration and reads in mapper

--- NaiveBayesClassifier/NaiveBayesDriver.java
+++ NaiveBayesClassifier/NaiveBayesDriver.java
@@ -12,23 +12,32 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
 
 /**
- * This class is a driver for mapreduce task used for training the naive bayes classifier.
- * Input path : path of training.txt file
- * Output path : path of output directory
+ * This class is a driver for map reduce task.
  * @author "Shubhangi Rakhonde, CS286, SJSU, Fall 2015"
- *
  */
 public class NaiveBayesDriver extends Configured implements Tool
 {
+	/**
+	 * Main function to run the map reduce job
+	 * @param args[0] = Training File path
+	 *        args[1] = Directory path of Testing Files
+	 *        args[2] = Directory path of output directory
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception
 	{
 		Configuration conf = new Configuration();
 		System.exit(ToolRunner.run(conf, new NaiveBayesDriver(), args));
 	}
 	
+	/**
+	 * This function checks the command line arguments and creates the map reduce job
+	 */
 	public int run(String[] args) throws Exception
 	{
 		 // check the CLI
+		System.out.println("Dont forget to Check the command line arguments");
+		getConf().set("trainingFile",args[0]);
 		Job job = new Job(getConf(), "build NB Classifier");
 		job.setJarByClass(NaiveBayesDriver.class);
 		job.setMapperClass(NaiveBayesMapper.class);
@@ -39,8 +48,8 @@ public class NaiveBayesDriver extends Configured implements Tool
 		job.setOutputValueClass(Text.class);
 		
 		//setup input and output paths
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		
 		//launch job synchronously
 		return job.waitForCompletion(true) ? 0:1;
