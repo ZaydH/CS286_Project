2a5798cd592db7f9cb4454dc5c1936a0a7570067
root <root@ip-10-0-0-147.us-west-2.compute.internal>
2015-11-25 05:53:15 -0500

    Fixing another bug in oozie_flow_aws for oozie ver. 4.2.0:

--- oozie_flow/oozie_flow_aws.bash
+++ oozie_flow/oozie_flow_aws.bash
@@ -3,7 +3,7 @@
 export SYSTEM_FULL_URL=ip-10-0-0-149.us-west-2.compute.internal
 
 export OOZIE_URL='http://${SYSTEM_FULL_URL}:11000/oozie'
-export OOZIE_HOME=/opt/mapr/oozie/oozie-4.1.0
+export OOZIE_HOME=/opt/mapr/oozie/oozie-4.2.0
 export PATH=$PATH:$OOZIE_HOME/bin
 
 $OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /home/ec2-user/CS286_Project/oozie_flow/job_aws.properties -run
