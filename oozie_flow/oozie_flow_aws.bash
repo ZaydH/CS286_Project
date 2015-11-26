b136944b7b8d8807b58ba21547cc723dcaadd1d4
ec2-user <ec2-user@ip-10-196-77-178.us-west-1.compute.internal>
2015-11-26 12:41:00 -0800

    Project running on the cluster from Casaletto.

--- oozie_flow/oozie_flow_aws.bash
+++ oozie_flow/oozie_flow_aws.bash
@@ -2,8 +2,8 @@
 
 #export SYSTEM_FULL_URL=ip-10-0-0-147.us-west-2.compute.internal
 
-export OOZIE_URL="http://ip-10-0-0-118.us-west-1.compute.internal:11000/oozie"
-export OOZIE_HOME=/opt/mapr/oozie/oozie-4.2.0
+export OOZIE_URL="http://ip-10-196-77-178:11000/oozie"
+export OOZIE_HOME=/opt/mapr/oozie/oozie-4.0.1
 export PATH=$PATH:$OOZIE_HOME/bin
 
 $OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /home/ec2-user/CS286_Project/oozie_flow/job_aws.properties -run
@@ -12,5 +12,5 @@ $OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /home/ec2-user/CS286_Pro
 #$OOZIE_HOME/bin/oozie job -oozie $OOZIE_URL -info <JobName>
 #$OOZIE_HOME/bin/oozie job -oozie $OOZIE_URL -log <JobName>
 
-#/opt/mapr/oozie/oozie-4.2.0/bin/oozie job -oozie $OOZIE_URL -info <JobName>
+#/opt/mapr/oozie/oozie-4.0.1/bin/oozie job -oozie $OOZIE_URL -info <JobName>
 
