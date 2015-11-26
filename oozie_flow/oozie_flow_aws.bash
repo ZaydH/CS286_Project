d757beda6e32785ad432b0e597a97f2530baea96
ec2-user <ec2-user@ip-10-0-0-118.us-west-1.compute.internal>
2015-11-26 11:26:28 -0500

    Fixes to the job tracker to the AWS properties file as it did not have the job tracker port.

--- oozie_flow/oozie_flow_aws.bash
+++ oozie_flow/oozie_flow_aws.bash
@@ -2,7 +2,7 @@
 
 #export SYSTEM_FULL_URL=ip-10-0-0-147.us-west-2.compute.internal
 
-export OOZIE_URL="http://ip-10-0-0-27.us-west-2.compute.internal:11000/oozie"
+export OOZIE_URL="http://ip-10-0-0-118.us-west-1.compute.internal:11000/oozie"
 export OOZIE_HOME=/opt/mapr/oozie/oozie-4.2.0
 export PATH=$PATH:$OOZIE_HOME/bin
 
@@ -11,3 +11,6 @@ $OOZIE_HOME/bin/oozie job -oozie="${OOZIE_URL}" -config /home/ec2-user/CS286_Pro
 # Check job status. Replace <JobName> with the actual job name
 #$OOZIE_HOME/bin/oozie job -oozie $OOZIE_URL -info <JobName>
 #$OOZIE_HOME/bin/oozie job -oozie $OOZIE_URL -log <JobName>
+
+#/opt/mapr/oozie/oozie-4.2.0/bin/oozie job -oozie $OOZIE_URL -info <JobName>
+
